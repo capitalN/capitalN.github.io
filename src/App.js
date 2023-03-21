5e5700c7c1e84f731329b6a4d1ac13c2a9da6b03
nikhil <angolkarnikhil@gmail.com>
2023-03-21 20:36:09 +0530

    new portfolio

--- src/App.js
+++ src/App.js
@@ -1,200 +1,199 @@
+// ChakraUI
 import {
   Box,
-  Button,
-  Flex,
   Heading,
-  Link,
-  useDisclosure,
+  HStack,
+  Stack,
+  Text,
+  VStack,
+  Flex,
   Show,
-  Hide,
   Drawer,
   DrawerBody,
-  DrawerOverlay,
-  Text,
   DrawerFooter,
-  Input,
-  DrawerCloseButton,
+  DrawerOverlay,
   DrawerContent,
-  DrawerHeader,
-  Stack,
-  HStack,
-  VStack,
-  Divider,
+  DrawerCloseButton,
+  useDisclosure,
+  Link,
+  Image,
 } from "@chakra-ui/react";
+
+// Components
+import Footer from "./components/Footer";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Home from "./components/Home";
+import Skills from "./components/Skills";
+
+// AOS
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-import About from "./Pages/About";
-import Contact from "./Pages/Contact";
-import Home from "./Pages/Home";
-import Projects from "./Pages/Projects";
-import Skills from "./Pages/Skills";
-import "./App.css";
-import React, { useEffect } from "react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+// React
+import { useEffect, useRef } from "react";
 
-const LINKS = [
-  { to: "#home", title: "HOME" },
-  { to: "#about", title: "ABOUT" },
-  { to: "#skills", title: "SKILLS" },
-  { to: "#projects", title: "PROJECTS" },
-  { to: "#contact", title: "CONTACT" },
-];
+// Icons
+import { TiThMenu } from "react-icons/ti";
 
-function App() {
+// Styles
+import { bgFontStyle } from "./components/styles";
+
+
+// Code
+export default function App({ data, repo }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const btnRef = React.useRef();
+  const btnRef = useRef();
+
+  const LINKS = [
+    { to: "#", title: "Home" },
+    { to: "#about", title: "About" },
+    { to: "#skills", title: "Skills" },
+    { to: "#projects", title: "Projects" },
+    { to: "#contact", title: "Contact" },
+  ];
 
   useEffect(() => {
     AOS.init({
-      offset: 200,
-      duration: 600,
-      easing: "ease-in-sine",
-      delay: 100,
+      disable: "mobile",
     });
   }, []);
 
   return (
-    <Box>
-      <Show above="md">
-        <Flex
-          as={"header"}
-          justify={"space-evenly"}
-          position="fixed"
-          w="100vw"
-          h={"60px"}
-          align={"center"}
-          bg={"black"}
-          color="white"
-          zIndex={10}
-          id="navbar"
-          transition={"top 0.3s"}
-        >
-          <Link href="#home" _hover={{ textDecoration: "none" }}>
-            <Heading fontFamily={"mono"}>NIKHIL</Heading>
-          </Link>
-          <Flex justify={"space-evenly"} w="50%">
-            {LINKS.map((link) => (
-              <Link
-                _hover={{ color: "orange" }}
-                fontWeight={"bold"}
-                href={link.to}
-                key={link.to}
-              >
-                {link.title}
-              </Link>
-            ))}
-          </Flex>
+    <VStack
+      color={"gray.300"}
+      bgColor={"black"}
+      // gap={{ base: "30px", sm: "40px", md: "50px", lg: "60px" }}
+      gap="50px"
+      textAlign="center"
+      p={{ base: "0 15px", sm: "0 20px", md: "0 30px", lg: "0 40px" }}
+      overflowX={"hidden"}
+      transform="0.2s ease"
+    >
+      <Box
+        fontWeight={"bold"}
+        position={"fixed"}
+        top="0"
+        w={"100%"}
+        p="10px 20px"
+        zIndex={100}
+        background="linear-gradient(180deg, black 10%, rgba(0,0,0,0) 100%)"
+        // id="navbar"
+      >
+        <Show above="md">
+          <HStack justify="space-between" align="center">
+            <a href="#">
+              <Image src="/nik.png" alt="LOGO" width={"100px"} opacity="0.7" />
+            </a>
+            <Flex w={"50%"} justify="space-between">
+              {LINKS.map((link) => (
+                <a href={link.to} key={link.to}>
+                  {link.title}
+                </a>
+              ))}
+            </Flex>
+            <Heading
+              as={Link}
+              href="https://drive.google.com/u/0/uc?id=1GtZiQIVtAi8ap6WSe38hKNHeB_c4T4eg&export=download"
+              fontWeight="bold"
+              size={"lg"}
+              fontFamily="inherit"
+              onClick={() =>
+                window.open(
+                  "https://drive.google.com/file/d/1GtZiQIVtAi8ap6WSe38hKNHeB_c4T4eg/view",
+                  "_blank"
+                )
+              }
+            >
+              RESUME {">"}
+            </Heading>
+          </HStack>
+        </Show>
 
-          <Button
-            borderRadius={"full"}
-            bg={"orange"}
-            color="white"
-            _hover={{ bg: "white", color: "black" }}
-            fontWeight="bold"
-            w="120px"
-            as={Link}
-            href="https://drive.google.com/u/0/uc?id=1GtZiQIVtAi8ap6WSe38hKNHeB_c4T4eg&export=download"
-            onClick={() =>
-              window.open(
-                "https://drive.google.com/file/d/1GtZiQIVtAi8ap6WSe38hKNHeB_c4T4eg/view",
-                "_blank"
-              )
-            }
-          >
-            RESUME
-          </Button>
-        </Flex>
-      </Show>
+        <Show below="md">
+          <Flex justify={"space-between"}>
+            <a href="#" fontWeight="bold">
+              <Image src="/nik.png" alt="LOGO" width={"75px"} opacity="0.7" />
+            </a>
+            <TiThMenu
+              ref={btnRef}
+              onClick={onOpen}
+              size="30px"
+              style={{ margin: "4px" }}
+            />
+            <Drawer
+              isOpen={isOpen}
+              placement="top"
+              onClose={onClose}
+              finalFocusRef={btnRef}
+            >
+              <DrawerOverlay />
+              <DrawerContent bgColor={"gray.900"}>
+                <Stack w="100%" h="100%" bgColor={" rgba(23,25,35, 0.6)"}>
+                  <DrawerCloseButton color={"white"} />
+                  <Flex justify={"center"} p="10px">
+                    <a href="#" fontWeight="bold" onClick={onClose}>
+                      <Image
+                        src="/nik.png"
+                        alt="LOGO"
+                        width={"100px"}
+                        opacity="0.7"
+                      />
+                    </a>
+                  </Flex>
 
-      <Show below="md">
-        <HStack
-          justify={"space-between"}
-          position={"fixed"}
-          zIndex={"100"}
-          w="100%"
-          bgColor={"black"}
-          p="10px"
-        >
-          <Heading as={Link} href="#home" fontFamily={"mono"} color="white">
-            NIKHIL
-          </Heading>
-          <Button
-            // right="4"
-            // top="4"
-            ref={btnRef}
-            onClick={onOpen}
-            borderRadius={"full"}
-            bg={"orange"}
-            color="white"
-            w="50px"
-            h="50px"
-          >
-            <HamburgerIcon fontSize={"20px"} fontWeight="bold" />
-          </Button>
-          <Drawer
-            isOpen={isOpen}
-            placement="top"
-            onClose={onClose}
-            finalFocusRef={btnRef}
-          >
-            <DrawerOverlay />
-            <DrawerContent bg={"black"} color="white">
-              <DrawerCloseButton />
-              <Heading fontFamily={"mono"}>NIKHIL</Heading>
-              <Divider />
-              <DrawerBody>
-                <Stack>
-                  {LINKS.map((link) => (
-                    <Link
-                      _hover={{ color: "orange" }}
-                      fontWeight={"bold"}
-                      href={link.to}
-                      key={link.to}
-                      textAlign="left"
-                    >
-                      {link.title}
-                    </Link>
-                  ))}
+                  <DrawerBody>
+                    <Stack gap="10px">
+                      {LINKS.map((link) => (
+                        <Link href={link.to} key={link.to} onClick={onClose}>
+                          <Text fontWeight={"bold"} color="gray.300">
+                            {link.title}
+                          </Text>
+                        </Link>
+                      ))}
+                    </Stack>
+                  </DrawerBody>
                 </Stack>
-              </DrawerBody>
-            </DrawerContent>
-          </Drawer>
-        </HStack>
-      </Show>
-
-      <Box color="black">
-        <Box id="home" bg="black" color={"white"}>
-          <Home />
-        </Box>
-        <Box id="about">
-          <About />
-        </Box>
-        <Box id="skills">
-          <Skills />
-        </Box>
-        <Box id="projects">
-          <Projects />
-        </Box>
-        <Box id="contact">
-          <Contact />
-        </Box>
+              </DrawerContent>
+            </Drawer>
+          </Flex>
+        </Show>
       </Box>
 
       <Box
+        position={"relative"}
+        bgImage={{ base: "spaceShip.jpg" }}
+        backgroundRepeat="no-repeat"
+        backgroundSize="cover"
+        bgPosition={"center"}
         w="100vw"
-        justify="space-evenly"
-        bg={"black"}
-        color="white"
-        padding={"20px"}
+        h="100vh"
+        id="#"
       >
-        <Heading>nikhil angolkar</Heading>
-        <Link href="mailto:angolkarnikhil@gmail.com">
-          angolkarnikhil@gmail.com
-        </Link>
+        <Home />
       </Box>
-    </Box>
+
+      {/* <p id="about"></p> */}
+      <VStack position={"relative"} id="about">
+        <Heading {...bgFontStyle}>ABOUT</Heading>
+        <About />
+      </VStack>
+
+      {/* <p id="skills"></p> */}
+      <VStack position={"relative"} id="skills">
+        <Heading {...bgFontStyle}>SKILLS</Heading>
+        <Skills />
+      </VStack>
+
+      {/* <p id="projects"></p> */}
+      <VStack position={"relative"} id="projects">
+        <Heading {...bgFontStyle}>PROJECTS</Heading>
+        <Projects />
+      </VStack>
+      <Box id="contact" zIndex={101}>
+        <Contact />
+      </Box>
+    </VStack>
   );
 }
-
-export default App;
